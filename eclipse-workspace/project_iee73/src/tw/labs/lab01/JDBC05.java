56e7f581ba35e0cb9d6ecacb14603019c1f2be87
GX <stken79s@gmail.com>
2023-12-05 09:03:36 +0800

    12/04

--- eclipse-workspace/project_iee73/src/tw/labs/lab01/JDBC05.java
+++ eclipse-workspace/project_iee73/src/tw/labs/lab01/JDBC05.java
@@ -7,6 +7,7 @@ import java.net.URLConnection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Properties;
 
@@ -21,51 +22,21 @@ public class JDBC05 {
 		prop.put("user", "root");
 		prop.put("password", "root");
 		try {
-			// 連接目標網站
-			URL url = new URL("https://data.moa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx?&UnitId=193");
-			URLConnection urlConn = url.openConnection();
-			System.out.println("目標網站連接成功");
-
 			// 連接 mysql
 			Connection sqlConn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/brad", prop);
 			System.out.println("資料庫連接成功");
-			String sql = "INSERT INTO food (name,addr,tel,pic,lat,lng) VALUES(?,?,?,?,?,?)";
-			PreparedStatement pstmt = sqlConn.prepareStatement(sql);
-
-			try (BufferedReader reader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));) {
-				String line;
-				StringBuffer sb = new StringBuffer();
-				while ((line = reader.readLine()) != null) {
-					sb.append(line);
-				}
-				JSONArray root = new JSONArray(sb.toString());
-				for (Object row : root) {
-					JSONObject data = (JSONObject) row;
-					String name = data.getString("Name");
-					String addr = data.getString("Address");
-					String tel = data.getString("Tel");
-					String pic = data.getString("PicURL");
-					String lat = data.getString("Latitude");
-					lat = lat.replaceAll("\\s", "");
-					if (lat == null || lat.isEmpty()) {
-						lat = "0.0";
-					}
-					String lng = data.getString("Longitude");
-					lng = lng.replaceAll("\\s", "");
-					if (lng == null || lng.isEmpty()) {
-						lng = "0.0";
-					}
-					// -----------
-					pstmt.setString(1, name);
-					pstmt.setString(2, addr);
-					pstmt.setString(3, tel);
-					pstmt.setString(4, pic);
-					pstmt.setDouble(5, Double.parseDouble(lat));
-					pstmt.setDouble(6, Double.parseDouble(lng));
-					pstmt.executeUpdate();
-				}
+			Statement stmt = sqlConn.createStatement();
+			String sql = "SELECT * FROM food";
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String name = rs.getString("name");
+				String lat = rs.getString("lat");
+				String lng = rs.getString("lng");
+				System.out.printf("%s : %s : %s : %s\n", id, name, lat, lng);
 			}
-			System.out.println("Done");
+
+			sqlConn.close();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
---
+++ eclipse-workspace/project_iee73/src/tw/labs/lab01/class64.java
@@ -0,0 +1,12 @@
+package tw.labs.lab01;
+
+public class class64 {
+
+	public static void main(String[] args) {
+		double a = 10.723;
+		System.out.println(Math.floor(a));
+		System.out.println(Math.ceil(a));
+
+	}
+
+}
